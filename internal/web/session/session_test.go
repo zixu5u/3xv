965cd553a8609e7a68bd8ed40c8a59e581d2ba92
zixu5u <33487755+zixu5u@users.noreply.github.com>
2026-06-14 22:57:51 +0800

    Update session_test.go

--- internal/web/session/session_test.go
+++ internal/web/session/session_test.go
@@ -5,7 +5,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/mhsanaei/3x-ui/v3/internal/database/model"
+	"github.com/zixu5u/3xv/v3/internal/database/model"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
