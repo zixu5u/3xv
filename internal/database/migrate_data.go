fafb06bf090a9fc775763b8be4ebdaa615957421
zixu5u <33487755+zixu5u@users.noreply.github.com>
2026-06-14 23:00:18 +0800

    Update migrate_data.go

--- internal/database/migrate_data.go
+++ internal/database/migrate_data.go
@@ -11,8 +11,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/mhsanaei/3x-ui/v3/internal/database/model"
-	"github.com/mhsanaei/3x-ui/v3/internal/xray"
+	"github.com/zixu5u/3xv/v3/internal/database/model"
+	"github.com/zixu5u/3xv/v3/internal/xray"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
