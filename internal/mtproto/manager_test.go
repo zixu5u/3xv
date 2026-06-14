afa019b97ef29c37d368f42efedd054bb1513dfe
zixu5u <33487755+zixu5u@users.noreply.github.com>
2026-06-14 23:02:26 +0800

    Update manager_test.go

--- internal/mtproto/manager_test.go
+++ internal/mtproto/manager_test.go
@@ -4,7 +4,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/mhsanaei/3x-ui/v3/internal/database/model"
+	"github.com/zixu5u/3xv/v3/internal/database/model"
 )
 
 func TestParseMetricLine(t *testing.T) {
